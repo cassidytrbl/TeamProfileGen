956fc0b9a11578d888bc541b8c07e0d58ed4914b
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-05-02 16:57:20 -0700

    adding variables

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
-const manager = require("manager");
-const engineer = require("engineer");
-const intern = require("intern");
 const path = require("path");
 const fs = require("fs");
+
+const manager = require("./employees/Manager");
+const engineer = require("./employees/Engineer");
+const intern = require("./employees/Intern");
+const engineerCard = require("./src/engineerCard");
+const internCard = require("./src/internCard");
