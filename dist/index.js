3a2c3ebb2030079407c6cfa87eb0c9474e5827a2
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-05-02 16:30:05 -0700

    adding the index.js

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const manager = require("manager");
+const engineer = require("engineer");
+const intern = require("intern");
+const path = require("path");
+const fs = require("fs");
