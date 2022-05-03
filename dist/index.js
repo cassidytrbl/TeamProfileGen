e0a1c12c42cdc1f4c749c40a260009002bdc47f5
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-05-02 17:40:37 -0700

    adding manager questions

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const manager = require("./employees/Manager");
-const engineer = require("./employees/Engineer");
-const intern = require("./employees/Intern");
-const engineerCard = require("./src/engineerCard");
-const internCard = require("./src/internCard");
---
+++ index.js
@@ -0,0 +1,63 @@
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const manager = require("./employees/Manager");
+const engineer = require("./employees/Engineer");
+const intern = require("./employees/Intern");
+const engineerCard = require("./src/engineerCard");
+const internCard = require("./src/internCard");
+
+let managerArray = [];
+let engineerArray = [];
+let internArray = [];
+
+function initPromptLoop() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What is your title?",
+      name: "position",
+      choices: ["Manager", "Engineer", "Intern"],
+    })
+    .then((choice) => {
+      const employeeRole = choice.position;
+      if (employeeRole === "Manager") {
+        initManagerQuestions();
+      } else if (employeeRole === "Engineer") {
+        initEngineerQuestions();
+      } else if (employeeRole === "Intern") {
+        initInternQuestions();
+      }
+    });
+}
+
+function initManagerQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter your name:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter your ID number:",
+      },
+    ])
+    .then(function (response) {
+      const newManager = new manager(
+        response.name,
+        response.id,
+        response.email
+      );
+      managerArray.push(newManager);
+      addNewEmployee();
+    });
+}
